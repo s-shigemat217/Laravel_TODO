e4bae5e6c89edad2b05f6d7acdd635378c65dac8
sgmt <s.shigematsu17@gmail.com>
2025-11-23 16:01:21 +0900

    Todoモデルにユーザーリレーションを追加し、ルーティングを設定

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,19 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'user_id',
         'title',
         'description',
         'is_completed',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
+
 }
